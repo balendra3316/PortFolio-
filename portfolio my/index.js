8a72460b2fbf2035887936f3850dc7ae60af982a
Balendra Singh Parihar <132284200+balendra3316@users.noreply.github.com>
2023-11-14 14:10:01 +0530

    Add files via upload

---
+++ portfolio my/index.js
@@ -0,0 +1,35 @@
+let cvBtn=document.getElementById("cvBtn");
+let submit=document.getElementById("submit");
+let bar=document.getElementById("bar");
+
+cvBtn.addEventListener("click",function(){
+    alert("hh")
+})
+
+submit.addEventListener("click",function(){
+     
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    if(email.value == "" && pass.value == ""){
+        alert("Fill Detail")
+    }else{
+        alert("Logged IN")
+    }
+
+})
+
+
+let ul=document.querySelector("ul")
+bar.addEventListener("click",function(){
+ul.classList.toggle("showdata");
+
+if(ul.className=="showData")
+{
+    bar.className="fa-solid fa-xmark"
+}else{
+   bar.className="fa-solid fa-bars"
+
+}
+
+})
